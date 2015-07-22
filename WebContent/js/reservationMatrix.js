ae11e5ad6d8abf0e87299c0967b6f549b650ed73
Monte Gardner <monte@gardnerweb.org>
2015-07-22 12:03:45 -0600

    no message

--- WebContent/js/reservationMatrix.js
+++ WebContent/js/reservationMatrix.js
@@ -8,17 +8,25 @@ App.directive("reservationMatrix",function(){
 			reservables: "=",
 			reservations: "="
 		},
+		/**
+		 * $scope.beginDate and $scope.endDate track the inner date range, the part that is visible within the viewport.
+		 * $scope.outerBeginDate and $scope.outerEndDate track the outer date range.  This is tracked so that we can load a larger date range
+		 * then is currently visible.  This way, the user can scroll left and right without having to immediately reload data.  After each 
+		 * left or right scroll, a new outer range is loaded. The inner date range should be maintained as one third of the outer date range, with 
+		 * each of the other 2/3rds being to the left and right.
+		 */
 		controller: function($scope,$timeout, $http){			
 			$scope.dateRange = new Array();
+			$scope.outerBeginDate = moment();
+			$scope.outerEndDate = moment();
 			
 			$scope.getReservables = function(){
 				var data = {
-						beginDate: $scope.beginDate.getTime(),
-						endDate: $scope.endDate.getTime()
+						beginDate: $scope.outerBeginDate.valueOf(),
+						endDate: $scope.outerEndDate.valueOf()
 				}
 				$http.get('ReservablesServlet', data).
-				success(function(data, status, headers, config) {		
-
+				success(function(data, status, headers, config) {
 					$scope.reservables = data;
 				});
 			};
@@ -40,17 +48,36 @@ App.directive("reservationMatrix",function(){
 			$scope.$watch("[beginDate.getTime(),endDate.getTime()]", function(oldValue, newValue){				
 				var begin=moment($scope.beginDate);
 				var end=moment($scope.endDate);			
-				$scope.dateRange = new Array();	            
-	            diff = end.diff(begin, 'days');
-
-	            $scope.dateRange.push(moment(begin).startOf("day"));
-	            for(var i = 0; i < diff; i++) {
-	            	$scope.dateRange.push(moment(begin.add(1,'d')));
+					            
+	            diff = end.diff(begin, "days");
+	            begin = begin.subtract(diff,"days");
+	            end = end.add(diff,"days");
+	            $scope.outerBeginDate = begin;
+	            $scope.outerEndDate = end;
+	            
+	            var indexMoment = moment(begin).startOf("day");
+	            $scope.dateRange = new Array();
+	            $scope.dateRange.push(moment(indexMoment));
+	            for(var i = 0; i < diff*3; i++) {
+	            	indexMoment.add("1","d");
+	            	$scope.dateRange.push(moment(indexMoment));
 	            }
-	            $timeout(function(){
-	    			var width = jQuery("#matrix").width() + 80;	    			
-	    			jQuery("#inner").css("width", width + "px");
-
+	            $timeout(function(){  
+	    			/*
+	            	var visibleWidth = jQuery("#wrapper").width();
+	    			var dateRangeCells = jQuery("#wrapper tr").last().find("td");
+	    			
+	    			var leftMostVisibleCell = jQuery(dateRangeCells[Math.floor(dateRangeCells.length/3)]);
+	    			var rightIndex = Math.floor(2*(dateRangeCells.length/3));
+	    			var rightMostVisibleCell = jQuery(dateRangeCells[rightIndex]);
+	    			
+	    			var scrollDistance = leftMostVisibleCell.offset().left - jQuery("#inner").offset().left;
+	    			jQuery("#inner").scrollLeft(scrollDistance);
+	    			var visibleCellWidth = Math.floor(visibleWidth/(dateRangeCells.length/3));
+	    			var entireWidth = visibleCellWidth * dateRangeCells.length;
+	    			jQuery("#matrix").css("width",entireWidth + "px");
+	    			*/
+	    				    			
 	            });
 	            
 
@@ -60,35 +87,38 @@ App.directive("reservationMatrix",function(){
 			$scope.getReservations = function(){
 	            var params = {
 		            	params: {
-		            		beginDate: $scope.beginDate.getTime(),
-		            		endDate: $scope.endDate.getTime()
+		            		beginDate: $scope.outerBeginDate.valueOf(),
+		            		endDate: $scope.outerEndDate.valueOf()
 		            	}
 		        }
 				$http.get("ReservationsServlet", params).
 	            success(function(data, status, headers, config) {
-					$scope.reservations = data;
+					$scope.reservations = data;					
 				});
 			};
 			
 			$scope.format = function(date){
-				return date.month() + "/" + date.date() + "/" + date.year();
+				return (date.month()+1) + "/" + date.date() + "/" + date.year();
 			}
 			$scope.isReserved = function(date, reservable){
-				var checkDate = moment(date);
-				for (var index=0; index < $scope.reservations.length; index++){
-					var checkReservation = $scope.reservations[index];
-					var checkStart = moment(checkReservation.beginDate);
-					var checkEnd = moment(checkReservation.endDate);
-					var matchedReservable = false;					
-					for (var index2=0; index2 < checkReservation.reservables.length; index2++){
-						if (checkReservation.reservables[index2].id == reservable.id){
-							matchedReservable=true;
+				if ($scope.reservations){
+					var checkDate = moment(date);
+					for (var index=0; index < $scope.reservations.length; index++){
+						var checkReservation = $scope.reservations[index];
+						var checkStart = moment(checkReservation.beginDate);
+						var checkEnd = moment(checkReservation.endDate);
+						var matchedReservable = false;					
+						for (var index2=0; index2 < checkReservation.reservables.length; index2++){
+							if (checkReservation.reservables[index2].id == reservable.id){
+								matchedReservable=true;
+							}
+						}
+						if (checkDate >= checkStart && checkDate <= checkEnd && matchedReservable){
+							return true;
 						}
 					}
-					if (checkDate >= checkStart && checkDate <= checkEnd && matchedReservable){
-						return true;
-					}
 				}
+				return false;
 			};
 			$scope.left = function(){
 				jQuery("#wrapper").kinetic("start", { velocity: -95 });
@@ -100,6 +130,8 @@ App.directive("reservationMatrix",function(){
 			
 		}, 
 		link: function($scope){
+			
+			
 			jQuery("#wrapper").kinetic({
 			    filterTarget: function(target, e){
 			    	target = jQuery(target);
@@ -109,6 +141,7 @@ App.directive("reservationMatrix",function(){
 			    	}
 			    }
 			});
+				
 			jQuery("#matrix").selectable({
 				filter: "td.spot",
 				stop: function(){
@@ -138,7 +171,7 @@ App.directive("reservationMatrix",function(){
 							var ed = moment(endDate);
 							for (var indexDate = moment(beginDate); indexDate <= ed; indexDate.add(1,"d")){
 								if ($scope.isReserved(indexDate, reservable)){
-									alert("Can't reservate that. Unit " + reservable.name + " is already reserved on " + $scope.format(indexDate));
+									alert("Can't reserve that. Unit " + reservable.name + " is already reserved on " + $scope.format(indexDate));
 									jQuery("#matrix td.spot").removeClass("ui-selected");
 									return false;
 								}
